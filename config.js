a50531bab999ddbcb514854437551a2e6952b3d2
Yariv <yariv.gdidi@gmail.com>
2021-08-09 01:56:52 +0300

    heroku fixes

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 const config = {
   ROOT_DIR: __dirname,
-  URL_PORT: 443,
-  URL_PATH: 'https://album-api-server.herokuapp.com',
+  URL_PORT: 8080,
+  URL_PATH: 'http://album-api-server.herokuapp.com',
   BASE_VERSION: '/api',
   CONTROLLER_DIRECTORY: path.join(__dirname, 'controllers'),
   PROJECT_DIR: __dirname,
